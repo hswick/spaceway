5212564ee6d3c5e392f3fbd9726bb8140c01749d
Harley Swick <hswick@smu.edu>
2015-02-28 15:00:20 -0600

    Looping background, tunnels are working

--- sketch.js
+++ sketch.js
@@ -1,16 +1,6 @@
   var tunnelCntr = 0;
-  var numberOfTunnels= 2;
+  var numberOfTunnels= 10;
   var tunnels = [];
-
-function tunnel(){
-  this.x = displayWidth / 2;
-  this.y = displayHeight / 2;
-  this.size = 0;
-  this.cntr = 0;
-  this.speed = 10;
-  this.drawing= false;
-  this.color = color(random(255), random(255), random(255));
-}
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
@@ -18,14 +8,27 @@ function setup(){
      tunnels[i] = new tunnel();
   }
   tunnels[0].drawing = true;
+  colors[0] = color(255, 0, 255);
+  colors[1] = color(0, 175, 239);
+  colors[2] = color(0, 255, 0);
 }
 
 function draw(){
-  background(255);
+  background(0);
   tunnelLoop();
   for(i = 0; i < numberOfTunnels; i++){
      drawTunnel(tunnels[i]);
   }
+}
+
+function tunnel(){
+  this.x = displayWidth / 2;
+  this.y = displayHeight / 2;
+  this.size = 0;
+  this.cntr = 0;
+  this.speed = 100;
+  this.drawing= false;
+  this.color = color(random(255), random(255), random(255));
 }
 
 function tunnelLoop(){
@@ -41,10 +44,27 @@ function tunnelLoop(){
   }
 }
 
+
 function drawTunnel(tunnel){
   if(tunnel.drawing){
-    fill(tunnel.color);
+    stroke(getColor());
+    strokeWeight(10);
+    noFill();
     ellipse(tunnel.x, tunnel.y, tunnel.size, tunnel.size);
     tunnel.size+=tunnel.speed;
   }
 }
+
+var colorCntr = 0;
+var numberOfColors = 3;
+var colors = [];
+function getColor(){
+  c = colors[colorCntr];
+  if(colorCntr == numberOfColors - 1){
+    colorCntr = 0;
+  }else{
+    colorCntr++;
+  }
+  return c;
+}
+
