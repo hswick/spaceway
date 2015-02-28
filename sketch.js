f0caa687078cfe68089113f22b0d3d054995c93a
Harley Swick <hswick@smu.edu>
2015-02-28 14:18:54 -0600

    Skeleton with initial p5

--- sketch.js
+++ sketch.js
@@ -1,15 +1,45 @@
-  var cntr = 0;
+  var tunnelCntr = 0;
   var speed;
+
+  var numberOfTunnels= 100;
+  var tunnels = [];
+
+function tunnel(){
+  this.x = displayWidth / 2;
+  this.y = displayHeight / 2;
+  this.size = 0;
+  this.cntr = 0;
+  this.speed = 10;
+  this.drawing= false;
+}
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  for(i = 0; i < numberOfTunnels; i++){
+     tunnels[i] = new tunnel();
+  }
+  tunnels[0].drawing = true;
 }
 
 function draw(){
   background(255);
-  speed = 5;
-  ellipse(displayWidth / 2, displayHeight / 2, cntr, cntr);
-  cntr+=speed;
-  if(cntr >= displayWidth)
-    cntr=0;
+  for(i = 0; i < numberOfTunnels; i++){
+     
+  }
+}
+
+
+
+function tunnelLoop(){
+  if(tunnels[tunnelCntr].size > displayWidth){
+    tunnels[tunnelCntr].drawing = false;
+    tunnelCntr++;
+  }
+}
+
+function drawTunnel(tunnel){
+  if(tunnel.drawing){
+    ellipse(tunnel.x, tunnel.y, tunnel.size, tunnel.size);
+    tunnel.size+=tunnel.speed;
+  }
 }
